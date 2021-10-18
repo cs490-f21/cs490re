d29d5a7148e26f74497824af967efca8b0d39773
jz579 <90346231+jz579@users.noreply.github.com>
2021-10-18 14:44:37 -0400

    ExamStatus timestamps

--- public_html/install/012_create_table_examstatus.sql
+++ public_html/install/012_create_table_examstatus.sql
@@ -3,7 +3,15 @@ CREATE TABLE IF NOT EXISTS ExamStatus (
     for_exam            INTEGER       NOT NULL, 
     from_student        INTEGER       NOT NULL,
     status              INTEGER       DEFAULT 0,
+    created             TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified            TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (for_exam) REFERENCES Exam(id),
     FOREIGN KEY (from_student) REFERENCES User(id)
 );
+--!!
+DROP TRIGGER IF EXISTS update_examstatus_modified ON ExamStatus;
+--!!
+CREATE TRIGGER update_examstatus_modified BEFORE UPDATE
+    ON ExamStatus FOR EACH ROW EXECUTE PROCEDURE 
+    update_modified_column();
